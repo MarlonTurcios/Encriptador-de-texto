e9684acf5e65b3623dda7d43b089bf194f9fbab7
Marlon Turcios <marlon_turcios97@hotmail.com>
2022-08-18 20:28:16 -0600

    Estandarizando nombres de clases caja resolución

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var btnEncriptar = document.getElementById("btnEncriptar");
 var btnDesencriptar = document.getElementById("btnDesencriptar");
 var btnCopiar = document.getElementById("btnCopiar");/* */
-var cajaResolucion = document.getElementById("cajaResolucion");
+var cajaResolucion = document.getElementById("seccionSinResolver");
 var textoResuelto = document.getElementById("textoResuelto");
 var seccionTextoResuelto = document.getElementById("seccionTextoResuelto");
 var codigoBase = document.getElementById("codigoBase");
@@ -19,3 +19,26 @@ function aparecer() {
 
 btnEncriptar.onclick = desaparecer;
 btnDesencriptar.onclick = aparecer;
+
+
+/*function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter",]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+        }
+        
+    }
+    return stringEncriptada;
+}
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(inputTexto.value);
+    mensaje.value = textoEncriptado;
+    mensaje.style.backgroundImage = "none";
+    inputTexto.value = textoEncriptado;
+    navigator.clipboard.writeText(mensaje.value);
+    mensaje.value = "";
+    alert("Texto copiado");
+}*/
